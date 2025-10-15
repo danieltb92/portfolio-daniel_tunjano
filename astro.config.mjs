7511cbd889610cbcad0e53df8bf39704a746603a
Daniel Tunjano <daniel.tunjano92@gmail.com>
2025-10-15 14:51:36 -0500

    firts commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
+import path from 'node:path';
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  //site: 'https://your-domain.com',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+     resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   }
 });
